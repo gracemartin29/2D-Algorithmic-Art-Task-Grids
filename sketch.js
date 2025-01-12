af56c82eb053a71c19e118450ddac4d2a3d4924e
gracemartin29 <grace.martin@student.norwichuni.ac.uk>
2025-01-12 19:46:39 +0000

    heart grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
+const CELLSIZE = 50
+const COLOURS = ["#330019", "#660033", "#99004C", "#CC0066", "#FF007F", "#FF3399", "#FF66B2", "#FF99CC", "#FFCCE5"]
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  // noLoop();
+  frameRate(3)
 }
 
 function draw() {
-  background(220);
+  background(0);
+
+  // basic heart grid
+  for (let y = 0; y < height; y += CELLSIZE) {
+    for (let x = 0; x < width; x += CELLSIZE) {
+      
+      arc(x + CELLSIZE/4, y + CELLSIZE/3, CELLSIZE/2, CELLSIZE/2, PI, 0)
+      arc(x + 3*CELLSIZE/4, y + CELLSIZE/3, CELLSIZE/2, CELLSIZE/2, PI, 0)
+      triangle(x, y+CELLSIZE/3, x+CELLSIZE/2, y+CELLSIZE, x+CELLSIZE, y+CELLSIZE/3)
+     
+      fill(random(COLOURS))
+      noStroke()
+
+    }
+  }
 }
